79368cab1dfbc54a74b03f2dcdec41478533be8b
DavidGomezD <davidgoomez7@gmail.com>
2020-09-27 23:30:20 -0500

    Anuncio de prueba colocado

--- app/src/main/java/com/davidgd/davidgd/yourgeographycountriesandcapitalsoftheworld/MainActivity.java
+++ app/src/main/java/com/davidgd/davidgd/yourgeographycountriesandcapitalsoftheworld/MainActivity.java
@@ -11,7 +11,17 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    //codigo de anuncio
+    private AdView mAdView;
 
     MediaPlayer mp;
 
@@ -29,6 +39,17 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+        //codigo del anuncio
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
 
         //Id de los botones
